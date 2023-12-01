bcf119e009a017326d0c4f057e8cd57f539f7cd9
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-12-01 17:16:32 -0300

    Add files via upload

---
+++ import.py
@@ -0,0 +1,15 @@
+# import caracteres as cr
+# print(cr.contar_vogais("Alguma coisa"))
+
+# from caracteres import contar_vogais
+# print(contar_vogais("Alguma coisa"))
+
+# from caracteres import (contar_vogais, contar_consoantes)
+# print(contar_vogais("Alguma coisa"),contar_consoantes("Alguma coisa"))
+
+# from caracteres import *
+
+# from numeros.numerais import *
+
+
+
